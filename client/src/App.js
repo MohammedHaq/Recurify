f6d0322f4b03a253b14429c0bb869aa8c114b031
MohammedHaq <mohammedzainhaq@gmail.com>
2023-10-05 21:15:04 -0400

    initial dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import ReactDom from 'react-dom'
 import './App.css';
 
-function App() {
+const mockClients = [
+  { id: 1, name: 'John Doe', lastRenewal: '2023-09-20' },
+  { id: 2, name: 'Jane Smith', lastRenewal: '2023-09-25' },
+  // ... more clients
+];
+
+const mockProducts = [
+  { id: 1, name: 'Product A', subscriberCount: 100 },
+  { id: 2, name: 'Product B', subscriberCount: 75 },
+  // ... more products
+];
+
+function Dashboard() {
+  const [clients] = useState(mockClients);
+  const [products] = useState(mockProducts);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dashboard">
+      <div className="clients-section">
+        <h2>Clients</h2>
+        <ul>
+          {clients.map(client => (
+            <li key={client.id}>
+              {client.name} (Last Renewal: {client.lastRenewal})
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="products-section">
+        <h2>Products</h2>
+        <ul>
+          {products.map(product => (
+            <li key={product.id}>
+              {product.name} (Subscribers: {product.subscriberCount})
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Other sections like Renewal Dates can be added here */}
+
     </div>
   );
-}
+};
 
-export default App;
+export default Dashboard;
