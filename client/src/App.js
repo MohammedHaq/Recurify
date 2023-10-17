711a3d6613f25f9b6aee16c3a4737c8b94a31249
MohammedHaq <mohammedzainhaq@gmail.com>
2023-10-17 00:13:02 -0400

    Update to draft UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,48 +2,28 @@ import React, { useState } from 'react'
 import ReactDom from 'react-dom'
 import './App.css';
 
-const mockClients = [
-  { id: 1, name: 'John Doe', lastRenewal: '2023-09-20' },
-  { id: 2, name: 'Jane Smith', lastRenewal: '2023-09-25' },
-  // ... more clients
+const Clients = [
+  // grab client info from database
 ];
 
-const mockProducts = [
-  { id: 1, name: 'Product A', subscriberCount: 100 },
-  { id: 2, name: 'Product B', subscriberCount: 75 },
-  // ... more products
+const Products = [
+  // grab product info from database
 ];
 
 function Dashboard() {
-  const [clients] = useState(mockClients);
-  const [products] = useState(mockProducts);
+  const [clients, addClient] = useState(Clients);
+  const [products, addProduct] = useState(Products);
 
   return (
     <div className="dashboard">
       <div className="clients-section">
+        <h1>Welcome, USER</h1>
         <h2>Clients</h2>
-        <ul>
-          {clients.map(client => (
-            <li key={client.id}>
-              {client.name} (Last Renewal: {client.lastRenewal})
-            </li>
-          ))}
-        </ul>
       </div>
 
       <div className="products-section">
         <h2>Products</h2>
-        <ul>
-          {products.map(product => (
-            <li key={product.id}>
-              {product.name} (Subscribers: {product.subscriberCount})
-            </li>
-          ))}
-        </ul>
       </div>
-
-      {/* Other sections like Renewal Dates can be added here */}
-
     </div>
   );
 };
